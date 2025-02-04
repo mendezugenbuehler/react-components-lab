ce41c320ef99ec5d7664f3c3f800d287f64003d7
Matt Mendez Zugenbuehler <mzugenbuehler@gmail.com>
2025-02-03 17:42:54 -0800

    including additional structure for WeatherData & WeatherIcon components

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,14 +1,14 @@
+import WeatherIcon from '../WeatherIcon/WeatherIcon';  
+import WeatherData from '../WeatherData/WeatherData'; 
 import './WeatherForecast.css';
 
 const WeatherForecast = ({ day, img, imgAlt, conditions, time }) => {
-    return (
-      <div className="weather">
-        <h2>{day}</h2>
-        <img src={img} alt={imgAlt} />
-        <p><span>conditions: </span>{conditions}</p>
-        <p><span>time: </span>{time}</p>
-      </div>
-    );
-  }
-  
-  export default WeatherForecast;
+  return (
+    <div className="weather">
+      <WeatherData day={day} conditions={conditions} time={time} />
+      <WeatherIcon img={img} imgAlt={imgAlt} />
+    </div>
+  );
+}
+
+export default WeatherForecast;
